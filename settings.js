9349ee9f4d20b9b0b438504de471ac0423d1a304
manjithdsilwa <111627071+manjithdsilwa@users.noreply.github.com>
2022-08-20 11:32:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94714204539']
+global.premium = ['94714204539']
+global.ownernomer = '94714204539'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
